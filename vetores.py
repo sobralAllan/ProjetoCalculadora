f5b4d1bba9aeb32d30be7279e78391a2499e43df
sobralAllan <39698566+sobralAllan@users.noreply.github.com>
2022-04-11 22:15:44 -0300

    Delete vetores.py
    adsgzgsgsgsg

--- vetores.py
+++
@@ -1,9 +0,0 @@
-vetor = []
-
-def preencherVetor():
-    for i in range(101):
-        vetor.append(i+1) #append = adicionar elementos dentro do vetor
-def mostrarVetor():
-    preencherVetor()
-    for i in range(101):
-        print(vetor[i])
